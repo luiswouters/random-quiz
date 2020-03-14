c8c49671ccfd1c200a27211e465a1e94029dee8b
Luis Wouters <luis.wouters@f2f-digital.com>
2020-03-14 10:03:44 -0300

    Updading install. Basic structure.

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,8 +1,21 @@
 import * as React from 'react';
 
+class Quiz extends React.Component {
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <h1>What's the meaning of life?</h1>
+                <label><input type='radio'/> True</label>
+                <button type='submit'>Next</button>
+            </div>
+        );
+    }
+}
+
 export const App = () => (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <h1>Lucid</h1>
-        <h2>Welcome to UI Team code assessment!</h2>
-    </div>
+    <Quiz />
 );
